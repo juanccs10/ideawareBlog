0b0e9bec50c0360de4e89bf7d1268207386dd48d
Juan C. Cuao Sanchez <juancamilocuao@gmail.com>
2019-10-12 15:27:59 -0500

    Updating Models

--- app/Post.php
+++ app/Post.php
@@ -7,6 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title','content','user_id'
+    ];
+
+    /**
      * Get all of the tags for the user.
      */
     public function tags()
--- app/Tag.php
+++ app/Tag.php
@@ -7,6 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name'
+    ];
+
+    /**
      * Get all of the posts that are assigned this tag.
      */
     public function posts()
