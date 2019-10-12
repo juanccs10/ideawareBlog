e0856fecca11d5a11f92426d5df2a45126d8bb7c
Juan C. Cuao Sanchez <juancamilocuao@gmail.com>
2019-10-12 14:39:27 -0500

    Added User Seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' => 'User Test',
+            'email' => 'user.test.12@mail.com',
+            'password' => bcrypt('123456'),
+        ]);
     }
 }
